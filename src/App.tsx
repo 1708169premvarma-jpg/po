dd766e1c493bbf566ffbfb10f673009fa111ea07
premvarma <1708169premvarma-jpg@users.noreply.github.com>
2026-02-09 11:52:17 +0530

    Fix: Correct case-sensitive import for Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import { Navbar } from './components/Navbar';
+import { Navbar } from './components/navbar';
 import { HomeSection } from './components/HomeSection';
 import { AboutSection } from './components/AboutSection';
 import { ProjectsSection } from './components/ProjectsSection';
